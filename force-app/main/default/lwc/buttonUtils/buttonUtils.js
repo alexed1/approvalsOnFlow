28859cc8bccd126004005a7dfc017396d6e49ca8
Andrii Kraiev <andkraiev@gmail.com>
2019-08-18 18:12:54 +0300

    Button disabling by field values; Converted logic to use Interfaces

--- force-app/main/default/lwc/buttonUtils/buttonUtils.js
+++ force-app/main/default/lwc/buttonUtils/buttonUtils.js
@@ -20,7 +20,6 @@ const generateCapabilityColumns = (labels, existingShares) => {
     });
 };
 
-//generate the object structure needed by Datatable, for a given label. 
 const getColumnDescriptor = (curButtonLabel) => {
     return {
         type: 'button',
@@ -28,85 +27,60 @@ const getColumnDescriptor = (curButtonLabel) => {
             label: curButtonLabel,
             name: curButtonLabel, //this is used to determine an apex method to call
             variant: 'neutral',
-            disabled: {fieldName: curButtonLabel.replace(' ','')+'buttonDisabled'}
+            disabled: {fieldName: curButtonLabel.replace(' ', '') + 'buttonDisabled'}
         },
         initialWidth: 85
     }
 };
 
-const buttonStyling = (supportedButtonSettings, selectedButtonNames ,id, existingShares) => {
+const buttonStyling = (supportedButtonSettings, selectedButtonNames, id, existingShares) => {
 
-    // it could be a group or a role on a group
     let existing = existingShares.find(
         share => {
             return share.Name === id;
         }
     );
 
-    // return {buttonDisabled: existing !== undefined}
     return buttonStylingSingle(supportedButtonSettings, selectedButtonNames, existing);
 };
 
 const buttonStylingSingle = (supportedButtonSettings, selectedButtonNames, existing) => {
-    //TODO: implement ability to utilize custom functions for button endbling/disabling
+
     let resultButtonSettings = {};
-        selectedButtonNames.replace(' ','').split(',').forEach(buttonName =>{
-            let bs = supportedButtonSettings.find(curSetting => curSetting.name == buttonName);
-            let isDisabled;
-            if(
-                (existing !== undefined && bs.matchingRule.matchingAction == 'EXISTS') ||
-                (existing === undefined && bs.matchingRule.matchingAction == 'NOTEXISTS')){
-                isDisabled = true;
-            }else {
-                isDisabled = false;
-            };
-            resultButtonSettings[buttonName.replace(' ','')+'buttonDisabled'] =isDisabled;
-        });
-        return resultButtonSettings;
+    selectedButtonNames.replace(' ', '').split(',').forEach(buttonName => {
+        let allbs = supportedButtonSettings.filter(curSetting => curSetting.name == buttonName);
+        let isDisabled = false;
+        if (allbs && allbs.length > 0) {
+            for (let i = 0; i < allbs.length; i++) {
+                if (
+                    (existing !== undefined && allbs[i].matchingRule.matchingAction == 'EXISTS') ||
+                    (existing === undefined && allbs[i].matchingRule.matchingAction == 'NOTEXISTS')) {
+                    isDisabled = true;
+                    break;
+                } else if (allbs[i].matchingRule.matchingAction == 'VALUEEQUALS') {
+                    let disabledValues = allbs[i].matchingRule.disabledValues;
+                    if (disabledValues) {
+                        for (var fieldName in disabledValues) {
+                            if (Object.prototype.hasOwnProperty.call(disabledValues, fieldName)) {
+                                disabledValues[fieldName].forEach(fieldValue => {
+                                    if (existing[fieldName] == fieldValue) {
+                                        isDisabled = true;
+                                    }
+                                });
+                            }
+                        }
+                        if (isDisabled) {
+                            break;
+                        }
+                    }
+                }
+            }
+        }
 
-    // // CEO ID: 00G9A0000011wy7UAA
-    // if (existing && existing.RowCause === 'Owner') {
-    //     return {
-    //         readDisabled: true,
-    //         editDisabled: true,
-    //         noneDisabled: true
-    //     };
-    // }
-    //
-    // const output = {
-    //     readDisabled: false,
-    //     editDisabled: false,
-    //     noneDisabled: false
-    // };
-    //
-    // if (existing) {
-    //     if (existing.AccessLevel === 'Read') {
-    //         output.readDisabled = true;
-    //     } else if (existing.AccessLevel === 'Edit') {
-    //         output.editDisabled = true;
-    //     }
-    // } else {
-    //     output.noneDisabled = true;
-    // }
-    //
-    // return output;
+        resultButtonSettings[buttonName.replace(' ', '') + 'buttonDisabled'] = isDisabled;
+    });
+    return resultButtonSettings;
 };
-
-
-// const shareDelete = async (UserOrGroupID, recordId) => {
-//     await deletePerm({
-//         UserOrGroupID,
-//         recordId
-//     });
-// };
-//
-// const shareUpdate = async (UserOrGroupID, recordId, level) => {
-//     await upsertPerm({
-//         UserOrGroupID,
-//         recordId,
-//         level
-//     });
-// };
 
 const handleButtonAction = async (buttonName, managerName, paramsString) => {
     await _handleButtonAction({
