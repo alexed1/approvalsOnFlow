b31691822ca6c30f21cf0b4cce4619a0a9ac6f06
Andrii Kraiev <andkraiev@gmail.com>
2019-09-13 19:26:35 +0300

    Role Manager + Ordering

---
+++ force-app/main/default/lwc/roleManager/roleManager.js
@@ -0,0 +1,132 @@
+import {LightningElement, api, wire, track} from 'lwc';
+
+import SupportedButtonsEmptyMessage from '@salesforce/label/c.SupportedButtonsEmptyMessage';
+import SupportedAddCapabilitiesEmptyMessage from '@salesforce/label/c.SupportedAddCapabilitiesEmptyMessage';
+import ButtonIsNotSupportedMessage from '@salesforce/label/c.ButtonIsNotSupportedMessage';
+import SupportedEditCapabilitiesEmptyMessage from '@salesforce/label/c.SupportedEditCapabilitiesEmptyMessage';
+import ManagerEmptyMessage from '@salesforce/label/c.ManagerEmptyMessage';
+
+import {refreshApex} from '@salesforce/apex';
+import getExistingMembers from '@salesforce/apex/RoleManagerController.getExistingMembers';
+import getSupportedButtons from '@salesforce/apex/RoleManagerController.getSupportedButtons';
+
+import {logger, logError} from 'c/lwcLogger';
+
+import {
+    getNotSupportedButtons
+} from 'c/buttonUtils';
+
+
+export default class RoleManager extends LightningElement {
+    @api myRecordId;
+    @api editTabName;
+    @api addTabName;
+    @api availableObjectTypes;
+    @api managerName;
+    @api supportedAddCapabilities;
+    @api supportedEditCapabilities;
+    @api layout = 'Tabbed';
+    @api memberParams;
+    @track existingMembers;
+    @track supportedButtons;
+    @api ruleName;
+    _refreshable;
+    @api log = false;
+    source = 'RoleManager';
+    @track loadFinished = false;
+    @api doRender;
+    @track cardTitle = '';
+    @track errors = [];
+
+    @wire(getSupportedButtons, {managerName: '$managerName'})
+    _getSupportedButtons(result) {
+        this._refreshable = result;
+        if (result.error) {
+            this.errors.push(result.error.body.message);
+            logError(this.log, this.source, 'getSupportedButtons', result.error);
+        } else if (result.data) {
+            this.supportedButtons = JSON.parse(result.data);
+            this.loadFinished = true;
+            if (this.doRender !== false) {
+                this.doRender = true;
+            }
+        }
+    }
+
+    @wire(getExistingMembers, {managerName: '$managerName', recordId: '$myRecordId'})
+    _getExistingMembers(result) {
+        this._refreshable = result;
+        if (result.error) {
+            this.errors.push(result.error.body.message);
+            logError(this.log, this.source, 'getExistingMembers', result.error);
+        } else if (result.data) {
+            this.existingMembers = result.data;
+            const memberRefreshedEvent = new CustomEvent('membersrefreshed', {
+                bubbles: true, detail: {
+                    members: result.data
+                }
+            });
+
+            this.dispatchEvent(memberRefreshedEvent);
+        }
+    }
+
+    get defaultTab() {
+        if (!this.existingMembers || this.existingMembers.length == 0) {
+            return this.addTabName;
+        } else {
+            return this.editTabName;
+        }
+
+    }
+
+    get showMarkup() {
+        return this.loadFinished && this.doRender;
+    }
+
+    @api refresh() {
+        refreshApex(this._refreshable).then(result => {
+            let cmpToRefresh = this.template.querySelector('c-add-new-members');
+            if (cmpToRefresh) {
+                cmpToRefresh.updateRowButtons();
+            }
+        });
+    }
+
+    get showExistingTab() {
+        return this.layout === 'Tabbed';
+    }
+
+    get errorMessage() {
+        let resultErrors = [...this.errors];
+
+        if (!this.supportedButtons) {
+            resultErrors.push(SupportedButtonsEmptyMessage);
+        }
+        if (!this.supportedAddCapabilities) {
+            resultErrors.push(SupportedAddCapabilitiesEmptyMessage);
+        }
+        if (!this.supportedEditCapabilities && this.showExistingTab) {
+            resultErrors.push(SupportedEditCapabilitiesEmptyMessage);
+        }
+        if (!this.managerName) {
+            resultErrors.push(ManagerEmptyMessage);
+        }
+
+        if (this.supportedButtons && (this.supportedAddCapabilities || this.supportedEditCapabilities)) {
+            let allButtonsString = this.supportedAddCapabilities + (this.showExistingTab ? (', ' + this.supportedEditCapabilities) : '');
+            let notSupportedButtnos = getNotSupportedButtons(this.supportedButtons, allButtonsString);
+
+            if (notSupportedButtnos.length > 0) {
+                resultErrors.push(ButtonIsNotSupportedMessage + notSupportedButtnos.join(', '));
+            }
+        }
+
+        if (resultErrors.length) {
+            return resultErrors.join('; ');
+        } else {
+            return false;
+        }
+
+    }
+}
---
+++ force-app/main/default/lwc/viewEditMembers/viewEditMembers.js
@@ -0,0 +1,65 @@
+import {LightningElement, api, track} from 'lwc';
+
+import {
+    buttonStyling,
+    generateCapabilityColumns,
+    handleButtonAction
+} from 'c/buttonUtils';
+
+import {logger, logError} from 'c/lwcLogger';
+
+export default class ViewEditMembers extends LightningElement {
+    @api myRecordId;
+    @api supportedEditCapabilities;
+    @api managerName;
+    @api existingMembers;
+    @api supportedButtons;
+
+    // @track columns = [];
+
+    // call this when you know the member table is out of sync
+    @api refresh() {
+        this.dispatchEvent(new CustomEvent('searchrefresh'));
+    }
+    //TODO: Implement ability to navigate to record on click
+    get tableData() {
+        const newArray = [];
+        if (this.existingMembers) {
+            this.existingMembers.forEach(result => {
+                newArray.push({
+                    ...result,
+                    ...buttonStyling(this.supportedButtons, this.supportedEditCapabilities, result.recordId, this.existingMembers)
+                });
+            });
+
+        }
+        return newArray;
+    }
+
+    get columns() {
+        return [{
+            label: 'Name',
+            fieldName: 'label'
+        }].concat(generateCapabilityColumns(this.supportedEditCapabilities));
+    }
+
+    async handleRowAction(event) {
+
+        //logger(this.log, this.source, 'row action called from datatable', event.detail);
+        let actionParams = JSON.stringify({
+            'userOrGroupID': event.detail.row.recordId,
+            'recordId': this.myRecordId
+        });
+
+        try {
+            await handleButtonAction(
+                event.detail.action.name,
+                this.managerName,
+                actionParams
+            );
+            this.refresh();
+        } catch (e) {
+            this.toastTheError(e, 'handleButtonAction');
+        }
+    }
+}
